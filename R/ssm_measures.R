fdd03676a86ae38372c070f3e1b13dcfad08e80e
Jeffrey Girard <j.girard@pitt.edu>
2017-12-03 10:56:17 -0600

    Create functions for correlational SSM parameters for measures

---
+++ R/ssm_measures.R
@@ -0,0 +1,82 @@
+#' Correlational Structural Summary Method
+#'
+#' Calculate SSM parameters with bootstrapped confidence intervals for any
+#' number of measures (based on their correlations with the circumplex scales).
+#'
+#' @param .data A matrix or data frame containing at least circumplex scales and
+#'   numeric measures to be evaluated.
+#' @param scales A list of variables in \code{.data} that contain circumplex
+#'   circumplex scales (in tidyverse-style NSE specification, see examples).
+#' @param measures A list of variables in \code{.data} that contain numeric
+#'   values (in tidyverse-style NSE specification, see examples).
+#' @param angles A numerical vector containing the angular displacement of each
+#'   circumplex scale included in \code{scales} (in degrees).
+#' @param boots The number of bootstrap resamples to use in calculating the
+#'   confidence intervals (default = 2000).
+#' @param interval The confidence intervals' percentage level (default = 0.95).
+#' @param plot A logical determining whether a plot should be created (default =
+#'   TRUE).
+#' @param ... Additional parameters to be passed to \code{ssm_plot()}.
+#' @return A tibble containing SSM parameters (point and interval estimates) for
+#'   each measure (based on their correaltions with the circumplex scales).
+
+ssm_measures <- function(.data, scales, measures, angles,
+  boots = 2000, interval = 0.95, plot = TRUE, ...) {
+  
+  # Enable column specification using tidyverse-style NSE -------------------
+  scales_en <- rlang::enquo(scales)
+  measures_en <- rlang::enquo(measures)
+  
+  # Check that inputs are valid ---------------------------------------------
+  assert_that(is.numeric(angles), is.count(boots))
+  assert_that(is.numeric(interval), interval > 0, interval < 1)
+  
+  # Iterate over measures to calculate SSM paramters ------------------------
+    data_scales <- .data %>%
+      dplyr::select(!!scales_en)
+    data_measures <- .data %>%
+      dplyr::select(!!measures_en)
+    results <- data_measures %>%
+      purrr::map_dfr(~ssm_measures_one(mutate(data_scales, measure = .),
+        angles, boots, interval), .id = "Measure")
+
+  results
+}
+
+#' Correlational Structural Summary Method
+#'
+#' Worker function for ssm_measures, calculates point and interval estimates for
+#' SSM parameters for a single measure.
+#'
+#' @param .data A matrix or data frame containing only circumplex scales and one
+#'   measure to be evaluated.
+#' @param angles A numerical vector containing the angular displacement of each
+#'   circumplex scale included in \code{scales} (in degrees).
+#' @param boots The number of bootstrap resamples to use in calculating the
+#'   confidence intervals (default = 2000).
+#' @param interval The confidence intervals' percentage level (default = 0.95).
+#' @return A tibble containing SSM parameters (point and interval estimates) for
+#'   the measure.
+
+ssm_measures_one <- function(.data, angles, boots, interval) {
+  
+  # Check that inputs are valid ---------------------------------------------
+  assert_that(are_equal(length(.data) - 1, length(angles)))
+  
+  # Get SSM parameter estimates for mean profile ----------------------------
+  rmat <- .data %>% cor()
+  scores <- rmat["measure", 1:(ncol(rmat) - 1)]
+  ssm <- ssm_parameters(scores, angles, tibble = FALSE)
+  
+  # Perform bootstrap on SSM parameters -------------------------------------
+  bs_function <- function(.data, index, angles) {
+    resample <- .data[index, ]
+    rmat_r <- .data %>% cor()
+    scores_r <- rmat["measure", 1:(ncol(rmat_r) - 1)]
+    ssm_r <- ssm_parameters(scores_r, angles, tibble = FALSE)
+    return(ssm_r)
+  }
+  results <- ssm_bootstrap(.data, bs_function, ssm, angles, boots, interval)
+  
+  results
+}
