633a2e863b3d37b31df57919c9004b1e60ea6f3d
Jeffrey Girard <j.girard@pitt.edu>
2017-12-10 12:13:52 -0600

    Export basic dplyr functions for easy of use

--- R/ssm.R
+++ R/ssm.R
@@ -19,5 +19,45 @@ NULL
 #' @usage lhs \%>\% rhs
 NULL
 
+#' Select variables by name
+#' 
+#' @name select
+#' @rdname select
+#' @keywords internal
+#' @export
+#' @importFrom dplyr select
+#' @usage select(.data, ...)
+NULL
+
+#' Rename variables by name
+#' 
+#' @name rename
+#' @rdname rename
+#' @keywords internal
+#' @export
+#' @importFrom dplyr rename
+#' @usage rename(.data, ...)
+NULL
+
+#' Return rows with matching conditions
+#' 
+#' @name filter
+#' @rdname filter
+#' @keywords internal
+#' @export
+#' @importFrom dplyr filter
+#' @usage filter(.data, ...)
+NULL
+
+#' Add new variables
+#' 
+#' @name mutate
+#' @rdname mutate
+#' @keywords internal
+#' @export
+#' @importFrom dplyr mutate
+#' @usage mutate(.data, ...)
+NULL
+
 #' @importFrom assertthat assert_that not_empty are_equal is.flag is.count
 NULL
