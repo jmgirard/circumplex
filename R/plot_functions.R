8ab20d3d562b21a15eb7aac9e24cbd1b3f79ed4f
Jeffrey Girard <j.girard@pitt.edu>
2017-12-20 16:48:49 -0600

    Add documentation and clean formatting on plot functions

--- R/plot_functions.R
+++ R/plot_functions.R
@@ -1,62 +1,147 @@
+#' Create an Empty Circular Plot
+#'
+#' Initialize an empty circular plot to be filled in by \code{circle_plot()}.
+#'
+#' @param angles A numerical vector containing the angular displacement of each
+#'   circumplex scale (in degrees). A line segment from the circle's origin to
+#'   perimeter and a label outside the perimeter will be drawn for each angle.
+#' @param amax A positive real number corresponding to the radius of the circle.
+#'   It is used to scale the amplitude values and will determine which amplitude
+#'   labels are drawn.
+#' @param font.size A positive real number corresponding to the size (in mm) of
+#'   the amplitude and displacement labels (default = 3).
+#' @return A ggplot variable containing an empty circular plot.
 
 circle_base <- function(angles = octants, amax = 0.5, font.size = 3) {
+
+  # Require plot to be square and remove default styling --------------------
+  b <- ggplot() + coord_fixed() + theme_void()
   
-  ggplot() + coord_fixed() + theme_void() +
+  # Expand the x-axis to fit long horizontal labels -------------------------
+  b <- b + scale_x_continuous(expand = c(.1, .1))  
+
+  # Draw circles corresponding to amplitude scale ---------------------------
+  b <- b +
     geom_circle(aes(x0 = 0, y0 = 0, r = 1:4), color = "gray", size = 0.5) + 
-    geom_circle(aes(x0 = 0, y0 = 0, r = 5), color = "darkgray", size = 1) +
+    geom_circle(aes(x0 = 0, y0 = 0, r = 5), color = "darkgray", size = 1)
+  
+  # Draw segments corresponding to displacement scale -----------------------
+  b <- b +
     geom_segment(
-      aes(x = 0, y = 0,
+      aes(
+        x = 0,
+        y = 0,
         xend = 5 * cos(angles * pi / 180),
         yend = 5 * sin(angles * pi / 180)
-      ),
-      color = "darkgray", size = 0.5
-    ) +
-    geom_label(
-      aes(x = 1:4, y = 0,
-        label = sprintf("%.2f", seq(from = 0, to = amax, length.out = 6)[2:5])),
-      color = "gray", label.size = NA, size = font.size
-    ) +
-    geom_label(
-      aes(x = 5.1 * cos(angles * pi / 180), y = 5.1 * sin(angles * pi / 180),
-        label = sprintf("%d\u00B0", angles)), size = font.size,
-      color = "darkgray", label.size = NA, hjust = "outward", vjust = "outward"
-    ) +
-    scale_x_continuous(expand = c(.1, .1))
+      ), 
+      color = "darkgray",
+      size = 0.5
+    )
   
+  # Draw labels for amplitude scale -----------------------------------------
+  b <- b + 
+    geom_label(
+      aes(
+        x = 1:4,
+        y = 0,
+        label = sprintf("%.2f", seq(from = 0, to = amax, length.out = 6)[2:5])
+      ),
+      color = "gray",
+      label.size = NA,
+      size = font.size
+    )
+
+  # Draw labels for displacement scale --------------------------------------
+  b <- b + 
+    geom_label(
+      aes(
+        x = 5.1 * cos(angles * pi / 180), y = 5.1 * sin(angles * pi / 180),
+        label = sprintf("%d\u00B0", angles)
+      ),
+      size = font.size,
+      color = "darkgray",
+      label.size = NA,
+      hjust = "outward",
+      vjust = "outward"
+    )
+  
+  b
 }
+
+#' Create a Circular Plot of SSM Results
+#'
+#' Take in the results of a Structural Summary Method analysis and plot the
+#' point and interval estimate for each row (e.g., group or measure) in a
+#' circular space quantified by displacement and amplitude.
+#'
+#' @param .results The output of \code{ssm_profiles()} or \code{ssm_measures()}.
+#' @param angles A numerical vector containing the angular displacement of each
+#'   circumplex scale (in degrees). A line segment from the circle's origin to
+#'   perimeter and a label outside the perimeter will be drawn for each angle.
+#' @param type A string corresponding to \code{"Profile"} or \code{"Measure"}.
+#' @param palette A string corresponding to the Color Brewer palette (default =
+#'   "Set1"). See http://www.cookbook-r.com/Graphs/Colors_(ggplot2).
+#' @param amax A positive real number corresponding to the radius of the circle.
+#'   It is used to scale the amplitude values and will determine which amplitude
+#'   labels are drawn.
+#' @param font.size A positive real number corresponding to the size (in mm) of
+#'   the amplitude and displacement labels (default = 3).
+#' @return A ggplot variable containing a completed circular plot.
 
 circle_plot <- function(.results, angles, type, palette = "Set1",
   amax = pretty_max(.results$a_uci), font.size = 3) {
   
-  df_plot <- .results %>% dplyr::mutate(
-    d_uci = ifelse(d_uci < d_lci && d_uci < 180, rwd(d_uci), d_uci),
-    d_lci = ifelse(d_lci > d_uci && d_lci > 180, rwd(d_lci), d_lci)
-  ) %>% 
-  mutate(
-    d_lci = ggrad(d_lci),
-    d_uci = ggrad(d_uci),
-    a_lci = a_lci * 10 / (2 * amax),
-    a_uci = a_uci * 10 / (2 * amax),
-    x_est = x_est * 10 / (2 * amax),
-    y_est = y_est * 10 / (2 * amax)
-  )
+  # Convert results to numbers usable by ggplot and ggforce -----------------
+  df_plot <- .results %>%
+    dplyr::rowwise() %>% 
+    dplyr::mutate(
+      d_uci = ifelse(d_uci < d_lci && d_uci < 180, rwd(d_uci), d_uci),
+      d_lci = ifelse(d_lci > d_uci && d_lci > 180, rwd(d_lci), d_lci),
+      d_uci = (d_uci - 90) * (-pi / 180),
+      d_lci = (d_lci - 90) * (-pi / 180),
+      a_lci = a_lci * 10 / (2 * amax),
+      a_uci = a_uci * 10 / (2 * amax),
+      x_est = x_est * 10 / (2 * amax),
+      y_est = y_est * 10 / (2 * amax)
+    )
+
+  # Initialize and configure the circle plot --------------------------------
   p <- circle_base(angles, amax, font.size) +
     scale_color_brewer(palette = palette) +
     scale_fill_brewer(palette = palette)
+  
   if (type == "Measure") {
-    p <- p + geom_arc_bar(data = df_plot,
-      aes(x0 = 0, y0 = 0, r0 = a_lci, r = a_uci, start = d_lci, end = d_uci,
-        fill = Measure, color = Measure),
-      alpha = 0.5, size = 1) +
-      geom_point(data = df_plot, aes(x = x_est, y = y_est, color = Measure),
-        size = 2)
+    # Draw point and interval estimates for each measure ----------------------
+    p <- p + 
+      geom_arc_bar(
+        data = df_plot,
+        aes(x0 = 0, y0 = 0, r0 = a_lci, r = a_uci, start = d_lci, end = d_uci,
+          fill = Measure, color = Measure),
+        alpha = 0.5,
+        size = 1
+      ) +
+      geom_point(
+        data = df_plot,
+        aes(x = x_est, y = y_est, color = Measure),
+        size = 2
+      )
   } else if (type == "Profile") {
-    p <- p + geom_arc_bar(data = df_plot,
-      aes(x0 = 0, y0 = 0, r0 = a_lci, r = a_uci, start = d_lci, end = d_uci,
-        fill = Group, color = Group),
-      alpha = 0.5, size = 1) +
-      geom_point(data = df_plot, aes(x = x_est, y = y_est, color = Group),
-        size = 2)
+    # Draw point and interval estimates for each group ------------------------
+    p <- p +
+      geom_arc_bar(
+        data = df_plot,
+        aes(x0 = 0, y0 = 0, r0 = a_lci, r = a_uci, start = d_lci, end = d_uci,
+          fill = Group, color = Group),
+        alpha = 0.5,
+        size = 1
+      ) +
+      geom_point(
+        data = df_plot,
+        aes(x = x_est, y = y_est, color = Group),
+        size = 2
+      )
+
+    # Remove the legend if there is only one group ----------------------------
     if (nlevels(.results$Group) == 1) {
       p <- p + theme(legend.position = "none")
     }
@@ -65,7 +150,18 @@ circle_plot <- function(.results, angles, type, palette = "Set1",
   p
 }
 
-diff_plot <- function(.results, interval) {
+#' Create a Difference Plot of SSM Results
+#'
+#' Take in the results of a Structural Summary Method analysis with pairwise
+#' contrasts and plot the point and interval estimates for each parameter's
+#' contrast (e.g., between groups or measures).
+#'
+#' @param .results The output of \code{ssm_profiles()} or \code{ssm_measures()}
+#'   that included contrasts (i.e., \code{pairwise = TRUE}).
+#' @return A ggplot variable containing difference point-ranges faceted by SSM
+#'   parameter. An interval that does not contain the value of zero has p<.05.
+
+diff_plot <- function(.results) {
   
   res <- .results %>%
     dplyr::mutate(
@@ -76,10 +172,16 @@ diff_plot <- function(.results, interval) {
     tidyr::extract(key, c("Parameter", "Type"), "(.)_(...)") %>%
     tidyr::spread(Type, value) %>%
     dplyr::rename(Estimate = est)
-  p <- ggplot(res) +
-    geom_pointrange(aes(x = Contrast, y = Estimate, ymin = lci, ymax = uci, color = Contrast)) +
+  p <- ggplot(res) + theme_bw() + theme(legend.position = "top") +
+    geom_pointrange(
+      aes(
+        x = Contrast, y = Estimate, ymin = lci, ymax = uci,
+        color = Contrast
+      ),
+      size = 1
+    ) +
     geom_hline(yintercept = 0) + 
-    facet_wrap(~Parameter, nrow = 1, scales = "free") +
-    theme_bw() + theme(legend.position = "top")
+    facet_wrap(~Parameter, nrow = 1, scales = "free")
+    
   p
 }
