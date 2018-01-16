899d05cb954739d33db47ab96f872b6114a62294
Jeffrey Girard <j.girard@pitt.edu>
2018-01-16 12:05:58 -0600

    Clean up imported functions

--- R/ssm.R
+++ R/ssm.R
@@ -9,14 +9,12 @@
 #' @name ssm
 NULL
 
-#' @importFrom assertthat are_equal assert_that is.count is.flag not_empty
+#' @importFrom assertthat
+#'   are_equal assert_that is.count is.flag not_empty is.scalar
 NULL
 
-#' @importFrom ggplot2 aes coord_fixed facet_wrap geom_hline geom_label
-#'   geom_point geom_pointrange geom_rect geom_segment ggplot scale_color_brewer
-#'   scale_fill_brewer scale_x_continuous scale_y_continuous theme theme_bw
-#'   theme_void xlab ylab
-NULL
-
-#' @importFrom ggforce geom_arc_bar geom_circle
+#' @importFrom ggplot2
+#'   aes coord_fixed facet_wrap geom_hline geom_label geom_point geom_pointrange
+#'   geom_rect geom_segment ggplot scale_color_brewer scale_fill_brewer
+#'   scale_x_continuous scale_y_continuous theme theme_bw theme_void xlab ylab
 NULL
