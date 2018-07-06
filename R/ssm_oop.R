451a6fca6378be2bcc397cb4a3714efcb1cace37
Jeffrey Girard <j.girard@pitt.edu>
2018-07-06 18:39:29 -0500

    Clean up S3 specifications

--- R/ssm_oop.R
+++ R/ssm_oop.R
@@ -27,22 +27,22 @@ new_degree <- function(x) {
 }
 
 # S3 generic for class 'degree'
-as_degree <- function(x) {
+as_degree <- function(x, ...) {
   UseMethod("as_degree")
 }
 
 # Set numeric object to class 'degree'
-as_degree.default <- function(x) {
+as_degree.default <- function(x, ...) {
   new_degree(x)
 }
 
 # Return object if already class 'degree'
-as_degree.degree <- function(x) {
+as_degree.degree <- function(x, ...) {
   x
 }
 
 # Convert from class 'radian' to class 'degree'
-as_degree.radian <- function(x) {
+as_degree.radian <- function(x, ...) {
   new_degree(x * (180 / pi))
 }
 
@@ -54,31 +54,26 @@ new_radian <- function(x) {
 }
 
 # S3 generic for class 'radian'
-as_radian <- function(x) {
+as_radian <- function(x, ...) {
   UseMethod("as_radian")
 }
 
 # Set numeric object to class 'radian'
-as_radian.default <- function(x) {
+as_radian.default <- function(x, ...) {
   new_radian(x)
 }
 
 # Return object if already class 'radian'
-as_radian.radian <- function(x) {
+as_radian.radian <- function(x, ...) {
   x
 }
 
 # Convert from class 'degree' to class 'radian'
-as_radian.degree <- function(x) {
+as_radian.degree <- function(x, ...) {
   new_radian(x * (pi / 180))
 }
 
 # Class ssm --------------------------------------------------------------------
-
-# S3 generic for class 'ssm'
-ssm <- function(x, ...) {
-  UseMethod("ssm")
-}
 
 # Constructor function
 new_ssm <- function(results, details, call, ...) {
