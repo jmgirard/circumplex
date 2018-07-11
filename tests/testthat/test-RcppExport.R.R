c1353faa47120498682f4acf9f11981c444e0ea0
Jeffrey Girard <j.girard@pitt.edu>
2018-07-11 15:32:25 -0500

    Further travis troubleshooting

--- tests/testthat/test-RcppExport.R.R
+++ tests/testthat/test-RcppExport.R.R
@@ -3,6 +3,11 @@ context("RcppExport.R")
 test_that("SSM parameters are correct", {
   data("aw2009")
   scores <- colMeans(aw2009)
-  res <- ssm_parameters(scores, octants())
-  expect_equal(round(res[5, 1], 3), 1.038)
+  res <- ssm_parameters(scores, as_radian(octants()))
+  
+  d_est_radian <- res[5, 1]
+  expect_equal(round(d_est_radian, 3), 6.010)
+  
+  d_est_degree <- as_degree(as_radian(d_est_radian))
+  expect_equal(as.vector(round(d_est_degree, 1)), 344.4)
 })
