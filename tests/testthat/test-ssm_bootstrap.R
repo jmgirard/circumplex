76c181521bbf7e07b91d8733a9d3c64de9a32689
jmgirard <j.girard@pitt.edu>
2018-07-20 11:24:44 -0400

    Add tests of quantile function for circular data

---
+++ tests/testthat/test-ssm_bootstrap.R
@@ -0,0 +1,26 @@
+context("test-ssm_bootstrap.R")
+
+test_that("Quantile for circular radians works", {
+  
+  a <- as_degree(as.double(0:180))
+  b <- as_radian(a)
+  qb <- quantile(b)
+  expect_type(qb, "double")
+  expect_s3_class(qb, "radian")
+  expect_equivalent(qb, as_radian(as_degree(c(0, 45, 90, 135, 180))))
+  
+  a <- as_degree(as.double(180:360))
+  b <- as_radian(a)
+  qb <- quantile(b)
+  expect_type(qb, "double")
+  expect_s3_class(qb, "radian")
+  expect_equivalent(qb, as_radian(as_degree(c(180, 225, 270, 315, 0))))
+  
+  a <- as_degree(as.double(c(270:360, 1:90)))
+  b <- as_radian(a)
+  qb <- quantile(b)
+  expect_type(qb, "double")
+  expect_s3_class(qb, "radian")
+  expect_equivalent(qb, as_radian(as_degree(c(270, 315, 0, 45, 90))))
+  
+})
