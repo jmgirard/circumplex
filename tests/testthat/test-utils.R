7925108e17b87a66e09c2e8258a9bea9c7a188ec
jmgirard <j.girard@pitt.edu>
2018-07-20 12:40:16 -0400

    Add tests of the pretty_max and angle convenience functions

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -6,3 +6,17 @@ test_that("Percentages are converted to strings properly", {
   expect_equal(str_percent(0.9999, 2), "99.99%")
   expect_equal(str_percent(0.99999, 2), "99.99%")
 })
+
+test_that("pretty max outputs the correct values", {
+  expect_warning(pretty_max(NA))
+  expect_equal(pretty_max(0), 0.05)
+  expect_equal(pretty_max(1), 1.25)
+  expect_equal(pretty_max(2.4), 2.5)
+  expect_equal(pretty_max(5), 8)
+})
+
+test_that("angle convenience functions work", {
+  expect_equal(octants(), as_degree(c(90, 135, 180, 225, 270, 315, 360, 45)))
+  expect_equal(poles(), as_degree(c(90, 180, 270, 360)))
+  expect_equal(quadrants(), as_degree(c(135, 225, 315, 45)))
+})
