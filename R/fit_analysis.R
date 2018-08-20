bf03daaa68c92c59bdd1888857789bfdb676eed6
Jeffrey Girard <me@jmgirard.com>
2018-08-20 00:05:51 -0400

    Adapt fisher and gap tests from psych package

--- R/fit_analysis.R
+++ R/fit_analysis.R
@@ -1,27 +1,21 @@
-
-
-nvar <- function(df, vars) {
-  vars_en <- rlang::enquo(vars)
-  df_sub <- dplyr::select(df, !!vars_en)
-  ncol(df_sub)
-}
-
-efa2 <- function(df, vars) {
-  vars_en <- rlang::enquo(vars)
-  df %>% 
-    dplyr::select(!!vars_en) %>% 
-    ipsatize(scales = !!vars_en) %>% 
-    stats::factanal(factors = 2, rotation = "none")
+#
+get_loadings <- function(.data, scales, nf, rot, fm) {
+  scales_en <- rlang::enquo(scales)
+  fit <- .data %>% 
+    dplyr::select(!!scales_en) %>% 
+    as.matrix() %>% 
+    psych::fa(nfactors = nf, rotate = rot, fm = fm)
+  fit$loadings
 }
 
 # Fisher Test of equal axes
-# less than .10 is almost certain equal axes
-# less than .15 is twice as likely
-# from Fisher 1997
-fit_fisher <- function(df, vars) {
-  lambda <- efa2(df, !!rlang::enquo(vars))$loadings
-  x <- lambda[, 1] ^ 2 + lambda[, 2] ^ 2
-  sd(x) / mean(x)
+# Best if the data were ipsatized
+# Reject unequal axes if less than .11 
+fit_fisher <- function(.data, scales) {
+  scales_en <- rlang::enquo(scales)
+  lambda <- get_loadings(.data, !!scales_en, nf = 2, rot = "none", fm = "pa")
+  radius <- (lambda[, 1] ^ 2) + (lambda[, 2] ^ 2)
+  sd(radius) / mean(radius)
 }
 
 # Gap Test of interstitiality
@@ -29,9 +23,13 @@ fit_fisher <- function(df, vars) {
 # less than .05 is twice as likely
 # affected by the number of variables
 # from Upton & Fingleton 1989
-fit_gap <- function(df, vars) {
-  lambda <- efa2(df, !!rlang::enquo(vars))$loadings
-  theta <- atan2(y = lambda[, 2], x = lambda[, 1])
+fit_gap <- function(.data, scales) {
+  scales_en <- rlang::enquo(scales)
+  lambda <- get_loadings(.data, !!scales_en, nf = 2, rot = "none", fm = "pa")
+  commun <- (lambda[, 1] ^ 2) + (lambda[, 2] ^ 2)
+  theta <- sign(lambda[, 2]) * acos(lambda[, 1] / sqrt(commun))
+  gaps <- diff(theta)
+  var(gaps)
 }
 
 # Variance Test 2 of interstitiality
@@ -44,6 +42,3 @@ fit_gap <- function(df, vars) {
 # less than .14 is almost certain interstitiality
 # less than .31 is twice as likely
 
-
-# Minkowski Test of interstitiality
-# best with large samples (n>600)
