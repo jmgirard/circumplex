9c166b94fa6cfaa64ec144715c117d5d83207d55
Jeffrey Girard <me@jmgirard.com>
2018-09-19 09:26:50 -0400

    Add instruments() function to load and list instruments

--- R/dat_oop.R
+++ R/dat_oop.R
@@ -22,7 +22,7 @@ print.instrument <- function(x, ...) {
       "circumplex measure of {x$Details$Construct} from {x$Details$Reference}."
     ),
     "\n"
-  ) 
+  )
 }
 
 #' @export
@@ -46,21 +46,21 @@ summary.instrument <- function(object, ...) {
 }
 
 #' Display the scales of a circumplex instrument
-#' 
+#'
 #' Display the scales of a circumplex instrument including the total number of
 #' scales and each scale's abbreviation, hypothetical angle, and text label.
-#' 
+#'
 #' @param x Required. An object of the instrument class.
 #' @return The same input object. Prints text to console.
 #' @family instrument functions
 #' @export
-#' @examples 
+#' @examples
 #' data(csip)
 #' scales(csip)
 
 scales <- function(x) {
   assert_that(is_instrument(x))
-  
+
   cat(
     glue(
       "The {x$Details$Abbrev} contains {x$Details$Scales} circumplex scales."
@@ -75,27 +75,27 @@ scales <- function(x) {
       "\n"
     )
   }
-  
+
   invisible(x)
 }
 
 #' Display the items of a circumplex instrument
-#' 
+#'
 #' Display the items of a circumplex instrument including the total number of
 #' items and each item's number and text. The item ordering/numbering displayed
 #' here is the same ordering/numbering assumed by the \code{score()} function.
-#' 
+#'
 #' @param x Required. An object of the instrument class.
 #' @return The same input object. Prints text to console.
 #' @family instrument functions
 #' @export
-#' @examples 
+#' @examples
 #' data(csip)
 #' items(csip)
 
 items <- function(x) {
   assert_that(is_instrument(x))
-  
+
   cat(
     glue(
       "The {x$Details$Abbrev} contains {x$Details$Items} items.\n"
@@ -109,27 +109,27 @@ items <- function(x) {
     }
     cat(glue("{xi$Text}"), "\n")
   }
-  
+
   invisible(x)
 }
 
 #' Display the anchors of a circumplex instrument
-#' 
+#'
 #' Display the anchors of a circumplex instrument including the total number of
 #' anchors and each anchor's numerical value and text label. Anchors are the
 #' response options that respondants select from (e.g., 0 = No, 1 = Yes).
-#' 
+#'
 #' @param x Required. An object of the instrument class.
 #' @return The same input object. Prints text to console.
 #' @family instrument functions
 #' @export
-#' @examples 
+#' @examples
 #' data(csip)
 #' anchors(csip)
 
 anchors <- function(x) {
   assert_that(is_instrument(x))
-  
+
   cat(
     glue(
       "The {x$Details$Abbrev} is rated using the following ",
@@ -137,7 +137,7 @@ anchors <- function(x) {
     ),
     "\n"
   )
-  for(i in seq_along(x$Anchors$Value)) {
+  for (i in seq_along(x$Anchors$Value)) {
     cat(
       glue(
         "{x$Anchors$Value[[i]]}. {x$Anchors$Label[[i]]}"
@@ -145,43 +145,43 @@ anchors <- function(x) {
       "\n"
     )
   }
-  
+
   invisible(x)
 }
 
 #' Display the norms for a circumplex instrument
-#' 
+#'
 #' Display the norms for a circumplex instrument including the total number of
-#' normative data sets available and each data set's number, sample size, 
+#' normative data sets available and each data set's number, sample size,
 #' population, and source reference and hyperlink. If another normative data set
 #' exists that is not yet included in the package, please let us know.
-#' 
+#'
 #' @param x Required. An object of the instrument class.
 #' @return The same input object. Prints text to console.
 #' @family instrument functions
 #' @export
-#' @examples 
+#' @examples
 #' data(csip)
 #' norms(csip)
 
 norms <- function(x) {
   assert_that(is_instrument(x))
-  
+
   samples <- x$Norms[[2]]
   n_norms <- nrow(samples)
-  
+
   if (n_norms == 0) {
     cat("The {x$Details$Abbrev} currently has no normative data sets.\n")
     return()
   }
-  
+
   cat(
     glue(
       "The {x$Details$Abbrev} currently has {n_norms} normative data set(s):"
     ),
     "\n"
   )
-  
+
   for (i in 1:n_norms) {
     sample_i <- samples$Sample[[i]]
     size_i <- samples$Size[[i]]
@@ -193,8 +193,55 @@ norms <- function(x) {
       "\n  ",
       glue("<{samples$URL[[i]]}>"),
       "\n"
-    )  
+    )
   }
-  
+
   invisible(x)
+}
+
+
+#' Load and list all circumplex instruments
+#'
+#' The circumplex package includes information about numerous circumplex
+#' instruments including instructions for scoring and standardizing items.
+#' Individual instruments can be loaded using the \code{data()} function, but
+#' this function can be used to quickly load (and list) all such instruments.
+#'
+#' @param load Optional. A logical determining whether the instrument objects
+#'   should be loaded into the environment (default = TRUE).
+#' @param display Optional. A logical determining whether a list of the
+#'   instruments hould be displayed in the console (default = TRUE).
+#' @family instrument functions
+#' @export
+#' @examples
+#' instruments()
+
+instruments <- function(load = TRUE, display = TRUE) {
+
+  # TODO: Find a way to automate this - maybe dir?
+
+  if (load == TRUE) {
+    data("csie")
+    data("csip")
+    data("csiv")
+    data("iip32")
+    data("iipsc")
+    data("iis64")
+    data("ipipipc")
+    data("isc")
+  }
+
+  if (display == TRUE) {
+    cat(
+      "The circumplex package currently includes 8 instruments:\n",
+      "1. CSIE: Circumplex Scales of Interpersonal Efficacy\n",
+      "2. CSIP: Circumplex Scales of Interpersonal Problems\n",
+      "3. CSIV: Circumplex Scales of Interpersonal Values\n",
+      "4. IIP-32: Inventory of Interpersonal Problems, Brief Version\n",
+      "5. IIP-SC: Inventory of Interpersonal Problems, Short Circumplex\n",
+      "6. IIS-64: Inventory of Interpersonal Strengths\n",
+      "7. IPIP-IPC: IPIP Interpersonal Circumplex\n",
+      "8. ISC: Interpersonal Sensitivities Circumplex\n"
+    )
+  }
 }
