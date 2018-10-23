ebbeb46c07c0f19ead2f953f3962295ac0f33e7b
jmgirard <me@jmgirard.com>
2018-10-22 21:33:13 -0400

    Add unit testing of ipsatize with overwrite and score with too few variables

--- tests/testthat/test-tidying_functions.R
+++ tests/testthat/test-tidying_functions.R
@@ -11,6 +11,16 @@ test_that("ipsatize works", {
   expect_equal(new$JK_i, c(2.0575, 0.5575, -0.2700, 0.7350, 0.5150))
   expect_equal(new$LM_i, c(1.3475, 1.5475, 1.3100, 0.9750, 0.5950))
   expect_equal(new$NO_i, c(-0.1625, 1.2875, 0.3400, -0.1450, -0.0450))
+  
+  ow <- ipsatize(aw2009, PA:NO, overwrite = TRUE)
+  expect_equal(ow$PA, c(-1.5225, 0.8975, 0.9800, -0.1550, -0.4450))
+  expect_equal(ow$BC, c(-1.4725, -1.2725, -0.5800, -1.0750, -0.5750))
+  expect_equal(ow$DE, c(-1.4025, -1.2025, -0.7300, -0.9150, -0.4650))
+  expect_equal(ow$FG, c(0.1775, -1.0225, -0.8900, -0.3650, 0.0650))
+  expect_equal(ow$HI, c(0.9775, -0.7925, -0.1600, 0.9450, 0.3550))
+  expect_equal(ow$JK, c(2.0575, 0.5575, -0.2700, 0.7350, 0.5150))
+  expect_equal(ow$LM, c(1.3475, 1.5475, 1.3100, 0.9750, 0.5950))
+  expect_equal(ow$NO, c(-0.1625, 1.2875, 0.3400, -0.1450, -0.0450))
 })
 
 test_that("score works", {
@@ -31,6 +41,7 @@ test_that("score works", {
   expect_equal(new$JK, c(2.50, 3.50, 2.00, 2.25, 2.00))
   expect_equal(new$LM, c(3.25, 1.25, 1.75, 1.50, 1.25))
   expect_equal(new$NO, c(2.75, 3.75, 2.75, 2.25, 1.00))
+  expect_error(score(old, X1:X30, instrument("iipsc")))
 })
 
 test_that("standardize works", {
